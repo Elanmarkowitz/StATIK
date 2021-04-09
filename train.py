ee7096d9c73d98706abe4325669808c866888299
keshavb96 <keshavb96@gmail.com>
2021-04-09 15:36:06 -0700

    Added DDP for naive data parallelism + data processing cleanup

--- train.py
+++ train.py
@@ -1,19 +1,17 @@
 # Branch keshav
 
-from absl import app, flags
-import tqdm
-import contextlib
+import os
 import pickle
 
 import torch
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
 import torch.nn.functional as F
-from torch.cuda.amp import autocast, GradScaler
-from ogb.lsc import WikiKG90MEvaluator
+import torch.optim as optim
+from absl import app, flags
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader, DistributedSampler
 
-from data.data_loading import load_dataset, WikiKG90MProcessedDataset, Wiki90MValidationDataset, Wiki90MTestDataset
+from data.data_loading import load_dataset, WikiKG90MProcessedDataset
 from model.kg_completion_gnn import KGCompletionGNN
 
 FLAGS = flags.FLAGS
@@ -25,6 +23,8 @@ flags.DEFINE_integer("layers", 2, "Number of message passing and edge update lay
 flags.DEFINE_integer("num_workers", 0, "Number of workers for the dataloader.")
 flags.DEFINE_float("lr", 1e-2, "Learning rate for optimizer.")
 flags.DEFINE_string("device", "cuda", "Device to use (cuda/cpu).")
+flags.DEFINE_integer("print_freq", 1024, "How frequently to print learning statistics in number of iterations")
+flags.DEFINE_integer("local_rank", 0, "How frequently to print learning statistics in number of iterations")
 
 DEBUGGING_MODEL = False
 
@@ -39,6 +39,7 @@ def prepare_batch_for_model(batch, dataset: WikiKG90MProcessedDataset, save_batc
         pickle.dump(batch, open('sample_batch.pkl', 'wb'))
     return batch
 
+
 def move_batch_to_device(batch, device):
     ht_tensor, ht_tensor_batch, r_tensor, entity_set, entity_feat, relation_feat, node_id_to_batch, queries, labels = batch
     ht_tensor_batch = ht_tensor_batch.to(device)
@@ -51,80 +52,63 @@ def move_batch_to_device(batch, device):
     return batch
 
 
+def train(global_rank, local_rank):
+    torch.cuda.set_device(local_rank)
+    dataset = load_dataset(FLAGS.root_data_dir)
+    sampler = DistributedSampler(dataset, rank=global_rank, shuffle=True)
+    train_loader = DataLoader(dataset, batch_size=FLAGS.batch_size,
+                              num_workers=FLAGS.num_workers, sampler=sampler,
+                              collate_fn=dataset.get_collate_fn(max_neighbors=FLAGS.samples_per_node))
+    model = KGCompletionGNN(dataset.num_relations, dataset.feature_dim, FLAGS.embed_dim, FLAGS.layers)
+    model.to(local_rank)
+    ddp_model = DDP(model, device_ids=[local_rank], find_unused_parameters=True)
+    opt = optim.Adam(ddp_model.parameters(), lr=FLAGS.lr)
+    moving_average_loss = torch.tensor(1.0)
+    moving_average_acc = torch.tensor(0.5)
+    moving_avg_rank = torch.tensor(10.0)
+    for i, batch in enumerate(train_loader):
+        ddp_model.train()
+        batch = prepare_batch_for_model(batch, dataset)
+        batch = move_batch_to_device(batch, local_rank)
+        ht_tensor, ht_tensor_batch, r_tensor, entity_set, entity_feat, relation_feat, node_id_to_batch, queries, labels = batch
+        preds = ddp_model(ht_tensor_batch, r_tensor, entity_feat, relation_feat, queries)
+        loss = F.binary_cross_entropy_with_logits(preds.flatten(), labels.float())
 
-def train():
-    DEVICE = torch.device(FLAGS.device)
-    scaler = GradScaler()
+        correct = torch.eq((preds > 0).long().flatten(), labels)
+        score_1 = preds[labels == 1].detach().cpu().flatten()[0]
+        score_0 = torch.topk(preds[labels == 0].detach().cpu().flatten().float()[:100], k=9).values
+        rank = 1 + (score_1 < score_0).sum()
+        moving_avg_rank = .9995 * moving_avg_rank + .0005 * rank.float()
 
-    if not DEBUGGING_MODEL:
-        dataset = load_dataset(FLAGS.root_data_dir)
-        train_loader = DataLoader(dataset, batch_size=FLAGS.batch_size, shuffle=True,
-                                  num_workers=FLAGS.num_workers,
-                                  collate_fn=dataset.get_collate_fn(max_neighbors=FLAGS.samples_per_node))
-        model = KGCompletionGNN(dataset.num_relations, dataset.feature_dim, FLAGS.embed_dim, FLAGS.layers)
-        model.to(DEVICE)
-        opt = optim.Adam(model.parameters(), lr=FLAGS.lr)
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(opt, mode='min', factor=0.5, patience=50,
-                                                         verbose=True, threshold_mode='rel', cooldown=10)
-        moving_average_loss = torch.tensor(1.0)
-        moving_average_acc = torch.tensor(0.5)
-        moving_avg_rank = torch.tensor(10.0)
-        for i, batch in tqdm.tqdm(enumerate(train_loader)):
-            model.train()
-            batch = prepare_batch_for_model(batch, dataset)
-            batch = move_batch_to_device(batch, DEVICE)
-            ht_tensor, ht_tensor_batch, r_tensor, entity_set, entity_feat, relation_feat, node_id_to_batch, queries, labels = batch
-            with autocast() if FLAGS.device == "cuda" else contextlib.suppress():
-                preds = model(ht_tensor_batch, r_tensor, entity_feat, relation_feat, queries)
-                loss = F.binary_cross_entropy_with_logits(preds.flatten(), labels.float())
+        training_acc = correct.float().mean()
+        moving_average_loss = .999 * moving_average_loss + 0.001 * loss.detach().cpu()
+        moving_average_acc = .99 * moving_average_acc + 0.01 * training_acc.detach().cpu()
 
-            correct = torch.eq((preds > 0).long().flatten(), labels)
-            score_1 = preds[labels==1].detach().cpu().flatten()[0]
-            score_0 = torch.topk(preds[labels==0].detach().cpu().flatten().float()[:100], k=9).values
-            rank = 1 + (score_1 < score_0).sum()
-            moving_avg_rank = .9995 * moving_avg_rank + .0005 * rank.float()
-
-            training_acc = correct.float().mean()
-            moving_average_loss = .999 * moving_average_loss + 0.001 * loss.detach().cpu()
-            moving_average_acc = .99 * moving_average_acc + 0.01 * training_acc.detach().cpu()
+        if i % FLAGS.print_freq == 0 and global_rank == 0:
             print(f"loss={loss.detach().cpu().numpy():.5f}, avg={moving_average_loss.numpy():.5f}, "
                   f"train_acc={training_acc.detach().cpu().numpy():.3f}, avg={moving_average_acc.numpy():.3f}, "
                   f"rank={rank} "
                   f"avg_rank={moving_avg_rank}")
-            opt.zero_grad()
-            scaler.scale(loss).backward() if FLAGS.device == "cuda" else loss.backward()
-            scaler.step(opt)
-            scaler.update()
-            scheduler.step(moving_average_loss)
+            moving_average_loss = torch.tensor(1.0)
+            moving_average_acc = torch.tensor(0.5)
+            moving_avg_rank = torch.tensor(10.0)
 
-    else:  # TODO: make part of this a function call to avoid code duplication
-        import pickle
-        batch = pickle.load(open('sample_batch.pkl', 'rb'))
-        batch = move_batch_to_device(batch, DEVICE)
-        ht_tensor, ht_tensor_batch, r_tensor, entity_set, entity_feat, relation_feat, node_id_to_batch, queries, labels = batch
-        model = KGCompletionGNN(1315, 768, FLAGS.embed_dim, FLAGS.layers)  # num ent 87143637
-        model.to(DEVICE)
-        opt = optim.Adam(model.parameters(), lr=FLAGS.lr)
-        model.train()
-        ht_tensor_batch = ht_tensor_batch.to(DEVICE)
-        r_tensor = r_tensor.to(DEVICE)
-        entity_feat = entity_feat.to(DEVICE)
-        relation_feat = relation_feat.to(DEVICE)
-        queries = queries.to(DEVICE)
-        labels = labels.to(DEVICE)
-        with autocast():
-            preds = model(ht_tensor_batch, r_tensor, entity_feat, relation_feat, queries)
-            loss = F.binary_cross_entropy_with_logits(preds.flatten(), labels.float())
-            breakpoint()
         opt.zero_grad()
-        scaler.scale(loss).backward()
-        scaler.step(opt)
-        scaler.update()
-        breakpoint()
+        loss.backward()
+        opt.step()
 
 
 def main(argv):
-    train()
+    os.environ['NCCL_SOCKET_IFNAME'] = 'ib0'
+    os.environ['GLOO_SOCKET_IFNAME'] = 'ib0'
+    grank = int(os.environ['RANK'])
+    ws = int(os.environ['WORLD_SIZE'])
+    master_addr = os.environ['MASTER_ADDR']
+    master_port = os.environ['MASTER_PORT']
+    dist.init_process_group(backend=dist.Backend.NCCL,
+                            init_method="tcp://{}:{}".format(master_addr, master_port), rank=grank, world_size=ws)
+    train(grank, FLAGS.local_rank)
+    dist.destroy_process_group()
 
 
 if __name__ == "__main__":
