d24bbeb16aa51f81ef3ae61be686e0e6427f8c17
keshavb96 <keshavb96@gmail.com>
2021-04-12 11:31:16 -0700

    Branch Merge 'main'

--- train.py
+++ train.py
@@ -2,16 +2,18 @@
 
 import os
 import pickle
+import numpy as np
 
 import torch
 import torch.distributed as dist
 import torch.nn.functional as F
 import torch.optim as optim
 from absl import app, flags
+from ogb.lsc import WikiKG90MEvaluator
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, DistributedSampler
 
-from data.data_loading import load_dataset, WikiKG90MProcessedDataset
+from data.data_loading import load_dataset, WikiKG90MProcessedDataset, Wiki90MValidationDataset
 from model.kg_completion_gnn import KGCompletionGNN
 
 FLAGS = flags.FLAGS
@@ -25,6 +27,7 @@ flags.DEFINE_float("lr", 1e-2, "Learning rate for optimizer.")
 flags.DEFINE_string("device", "cuda", "Device to use (cuda/cpu).")
 flags.DEFINE_integer("print_freq", 1024, "How frequently to print learning statistics in number of iterations")
 flags.DEFINE_integer("local_rank", 0, "How frequently to print learning statistics in number of iterations")
+flags.DEFINE_integer("validate_every", 1024, "How many iterations to do between each single batch validation.")
 
 DEBUGGING_MODEL = False
 
@@ -84,18 +87,63 @@ def train(global_rank, local_rank):
         moving_average_loss = .999 * moving_average_loss + 0.001 * loss.detach().cpu()
         moving_average_acc = .99 * moving_average_acc + 0.01 * training_acc.detach().cpu()
 
-        if i % FLAGS.print_freq == 0 and global_rank == 0:
+        if (i + 1) % FLAGS.print_freq == 0 and global_rank == 0:
             print(f"loss={loss.detach().cpu().numpy():.5f}, avg={moving_average_loss.numpy():.5f}, "
                   f"train_acc={training_acc.detach().cpu().numpy():.3f}, avg={moving_average_acc.numpy():.3f}, "
                   f"rank={rank} "
                   f"avg_rank={moving_avg_rank}")
+
             moving_average_loss = torch.tensor(1.0)
             moving_average_acc = torch.tensor(0.5)
             moving_avg_rank = torch.tensor(10.0)
 
+        if (i + 1) % FLAGS.validate_every == 0 and global_rank == 0:
+            ddp_model.eval()
+            validate(dataset, ddp_model.module, single_itr=True)
+
         opt.zero_grad()
         loss.backward()
         opt.step()
+
+
+# Validating only on global rank 0 for now
+def validate(dataset: WikiKG90MProcessedDataset, model: KGCompletionGNN, single_itr=False):
+    evaluator = WikiKG90MEvaluator()
+    valid_dataset = Wiki90MValidationDataset(dataset)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=100, num_workers=0, shuffle=True,
+                                  collate_fn=valid_dataset.get_eval_collate_fn(max_neighbors=FLAGS.samples_per_node))
+    top_10s = []
+    t_corrects = []
+    with torch.no_grad():
+        for i, (batch, t_correct_index) in enumerate(valid_dataloader):
+            batch_preds = []
+            for subbatch in batch:
+                subbatch = prepare_batch_for_model(subbatch, valid_dataset.ds)
+                subbatch = move_batch_to_device(subbatch, 0)
+                ht_tensor, ht_tensor_batch, r_tensor, entity_set, entity_feat, relation_feat, node_id_to_batch, queries, labels = subbatch
+                preds = model(ht_tensor_batch, r_tensor, entity_feat, relation_feat, queries)
+                batch_preds.append(preds)
+
+            batch_preds = torch.cat(batch_preds, dim=1)
+            t_pred_top10 = batch_preds.topk(10).indices
+            t_pred_top10 = t_pred_top10.detach().cpu().numpy()
+            batch_input_dict = {'h,r->t': {'t_pred_top10': t_pred_top10, 't_correct_index': t_correct_index}}
+            top_10s.append(t_pred_top10)
+            t_corrects.append(t_correct_index)
+            # batch_result_dict = evaluator.eval(batch_input_dict)
+            # print(batch_result_dict)
+            if single_itr:
+                break
+
+    t_pred_top10 = np.concatenate(top_10s, axis=0)
+    t_correct_index = np.concatenate(t_corrects, axis=0)
+    input_dict = {'h,r->t': {'t_pred_top10': t_pred_top10, 't_correct_index': t_correct_index}}
+    result_dict = evaluator.eval(input_dict)
+    print(result_dict)
+
+
+def test(dataset):
+    pass
 
 
 def main(argv):
