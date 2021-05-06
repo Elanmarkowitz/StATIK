0a0563a1c362e122fb53fc29fd1d952702b5c7be
Elan Markowitz <esmarkow@usc.edu>
2021-05-05 19:41:52 -0700

    test inference

--- train.py
+++ train.py
@@ -1,12 +1,10 @@
 import math
 import random
-import time
 
 from absl import app, flags
 import os
 import pickle
 import numpy as np
-import pdb
 
 import setproctitle
 
@@ -19,33 +17,37 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import Dataset, DataLoader, DistributedSampler, Subset
 from tqdm import tqdm
 
-from data.data_loading import load_dataset, WikiKG90MProcessedDataset, Wiki90MValidationDataset
+from data.data_loading import load_dataset, WikiKG90MProcessedDataset, Wiki90MValidationDataset, Wiki90MEvaluationDataset, Wiki90MTestDataset
 from model.kg_completion_gnn import KGCompletionGNN, TransELoss
 from utils import load_model, load_opt_checkpoint, save_checkpoint, save_model
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("root_data_dir", "/nas/home/elanmark/data", "Root data dir for installing the ogb dataset")
+flags.DEFINE_bool("distributed", True, "Indicate whether to use distributed training.")
+flags.DEFINE_integer("num_workers", 0, "Number of workers for the dataloader.")
+flags.DEFINE_integer("local_rank", 0, "How frequently to print learning statistics in number of iterations") # TODO: Change description here
+flags.DEFINE_integer("print_freq", 1024, "How frequently to print learning statistics in number of iterations")
+flags.DEFINE_string("device", "cuda", "Device to use (cuda/cpu).")
+flags.DEFINE_string("checkpoint", None, "Resume training from checkpoint file in checkpoints directory.")
+flags.DEFINE_string("name", "run01", "A name to use for saving the run.")
+
 flags.DEFINE_integer("batch_size", 100, "Batch size. Number of triples.")
 flags.DEFINE_integer("samples_per_node", 10, "Number of neighbors to sample for each entity in a query triple.")
 flags.DEFINE_integer("embed_dim", 256, "Number of dimensions for hidden states.")
 flags.DEFINE_integer("layers", 2, "Number of message passing and edge update layers for model.")
-flags.DEFINE_integer("num_workers", 0, "Number of workers for the dataloader.")
 flags.DEFINE_float("lr", 1e-2, "Learning rate for optimizer.")
-flags.DEFINE_string("device", "cuda", "Device to use (cuda/cpu).")
-flags.DEFINE_integer("print_freq", 1024, "How frequently to print learning statistics in number of iterations")
-flags.DEFINE_integer("local_rank", 0, "How frequently to print learning statistics in number of iterations")
 flags.DEFINE_integer("validate_every", 1024, "How many iterations to do between each single batch validation.")
 flags.DEFINE_bool("edge_attention", False, "Whether or not to attend to edges during ")
 flags.DEFINE_integer("validation_batches", 1000, "Number of batches to do for each validation check.")
 flags.DEFINE_integer("valid_batch_size", 1, "Batch size for validation (does all t_candidates at once).")
 flags.DEFINE_integer("epochs", 1, "Num epochs to train for")
 flags.DEFINE_float("margin", 1.0, "Margin in transE loss")
-flags.DEFINE_bool('inference_only', False, 'Whether or not to do a complete inference run across the validation set')
+
+flags.DEFINE_bool('validation_only', False, 'Whether or not to do a complete inference run across the validation set')
+flags.DEFINE_bool('test_only', False, 'Whether or not to do a complete inference run across the test set')
 flags.DEFINE_string('model_path_depr', None, 'DEPRECATED: Path where the model is saved')
 flags.DEFINE_string('model_path', None, 'Path where the model is saved (inference only)')
-flags.DEFINE_bool("distributed", True, "Indicate whether to use distributed training.")
-flags.DEFINE_string("checkpoint", None, "Resume training from checkpoint file in checkpoints directory.")
-flags.DEFINE_string("name", "run01", "A name to use for saving the run.")
+flags.DEFINE_string("test_save_dir", "test_submissions", "Directory to save test results file in.")
 
 CHECKPOINT_DIR = "checkpoints"
 
@@ -204,25 +206,30 @@ def train_single(device):
 
 
 def inference_only(global_rank, local_rank, world):
+    assert FLAGS.test_only or FLAGS.validation_only, "Must run validation set or test set if running inference."
+    assert not (FLAGS.test_only and FLAGS.validation_only), "Can only run on one of test or validation when doing inference."
     torch.cuda.set_device(local_rank)
     torch.manual_seed(0)
     random.seed(0)
     np.random.seed(0)
 
-    dataset = load_dataset(FLAGS.root_data_dir)
-    valid_dataset = Wiki90MValidationDataset(dataset)
+    base_dataset = load_dataset(FLAGS.root_data_dir)
+    if FLAGS.validation_only:
+        eval_dataset = Wiki90MValidationDataset(base_dataset)
+    else:
+        eval_dataset = Wiki90MTestDataset(base_dataset)
 
     num_ranks = world.size()
-    idxs_per_rank = len(valid_dataset) / num_ranks
+    idxs_per_rank = len(eval_dataset) / num_ranks
     start_idx = global_rank * idxs_per_rank
-    end_idx = (global_rank + 1) * idxs_per_rank if ((global_rank + 1) * idxs_per_rank <= len(valid_dataset)) else len(valid_dataset)
+    end_idx = (global_rank + 1) * idxs_per_rank if ((global_rank + 1) * idxs_per_rank <= len(eval_dataset)) else len(eval_dataset)
     rank_idxs = torch.arange(start_idx, end_idx, dtype=torch.long).tolist()
 
-    subset = Subset(valid_dataset, rank_idxs)
+    subset = Subset(eval_dataset, rank_idxs)
     valid_dataloader = DataLoader(subset, batch_size=FLAGS.valid_batch_size, num_workers=FLAGS.num_workers,
-                                   collate_fn=valid_dataset.get_eval_collate_fn(max_neighbors=FLAGS.samples_per_node))
+                                   collate_fn=eval_dataset.get_eval_collate_fn(max_neighbors=FLAGS.samples_per_node))
 
-    model = KGCompletionGNN(dataset.relation_feat, dataset.feature_dim, FLAGS.embed_dim, FLAGS.layers, edge_attention=FLAGS.edge_attention)
+    model = KGCompletionGNN(base_dataset.relation_feat, base_dataset.feature_dim, FLAGS.embed_dim, FLAGS.layers, edge_attention=FLAGS.edge_attention)
 
     if global_rank == 0:
         assert FLAGS.model_path_depr is not None or FLAGS.model_path is not None, 'Must be supplied with model to do inference.'
@@ -235,72 +242,89 @@ def inference_only(global_rank, local_rank, world):
     ddp_model = DDP(model, device_ids=[local_rank])
     ddp_model.eval()
 
-    if FLAGS.validation_batches < 0:
+    if FLAGS.test_only or FLAGS.validation_batches < 0:
         FLAGS.validation_batches = None
-        gather_sizes = [math.floor(len(valid_dataset) / num_ranks)] * (num_ranks - 1)
-        last_size = len(valid_dataset) - sum(gather_sizes)
+        gather_sizes = [math.floor(len(eval_dataset) / num_ranks)] * (num_ranks - 1)
+        last_size = len(eval_dataset) - sum(gather_sizes)
         gather_sizes.append(last_size)
     else:
         gather_sizes = [FLAGS.valid_batch_size * FLAGS.validation_batches] * num_ranks
 
-    result = validate(valid_dataset, valid_dataloader, ddp_model, global_rank, local_rank, gather_sizes, FLAGS.validation_batches, world)
+    result = validate(eval_dataset, valid_dataloader, ddp_model, global_rank, local_rank, gather_sizes, FLAGS.validation_batches, world)
     if global_rank == 0:
         mrr = result['mrr']
         print('Validation MRR = {}'.format(mrr))
 
 
-def validate(valid_dataset: Dataset, valid_dataloader: DataLoader, model, global_rank: int, local_rank: int, gather_sizes: list, num_batches: int = None,
-             world=None):
-    evaluator = WikiKG90MEvaluator()
-
+def run_inference(dataset: Wiki90MEvaluationDataset, dataloader: DataLoader, model, global_rank: int, local_rank: int,
+                  gather_sizes: list, num_batches: int = None, world=None):
     top_10s = []
     t_corrects = []
     with torch.no_grad():
-        for i, (batch, t_correct_index) in enumerate(tqdm(valid_dataloader)):
-            batch = prepare_batch_for_model(batch, valid_dataset.ds)
+        for i, (batch, t_correct_index) in enumerate(tqdm(dataloader)):
+            batch = prepare_batch_for_model(batch, dataset.ds)
             batch = move_batch_to_device(batch, local_rank)
             ht_tensor, r_tensor, entity_set, entity_feat, queries, _, r_queries, r_relatives, h_or_t_sample = batch
             preds = model(ht_tensor, r_tensor, entity_feat, queries)
             preds = preds.reshape(-1, 1001)
             t_pred_top10 = preds.topk(10).indices
             t_pred_top10 = t_pred_top10.detach()
-            t_correct_index = torch.tensor(t_correct_index, device=local_rank)
             top_10s.append(t_pred_top10)
-            t_corrects.append(t_correct_index)
+            if isinstance(dataset, Wiki90MValidationDataset):
+                t_correct_index = torch.tensor(t_correct_index, device=local_rank)
+                t_corrects.append(t_correct_index)
             if num_batches and num_batches == (i + 1):
                 break
 
     t_pred_top10 = torch.cat(top_10s, dim=0)
-    t_correct_index = torch.cat(t_corrects, dim=0)
-    aggregated_top10_preds, aggregated_correct_indices = gather_results(t_pred_top10, t_correct_index, global_rank, gather_sizes, world)
+    aggregated_top10_preds = gather_results(t_pred_top10, global_rank, gather_sizes, world)
+
+    aggregated_correct_indices = None
+    if isinstance(dataset, Wiki90MValidationDataset):
+        t_correct_index = torch.cat(t_corrects, dim=0)
+        aggregated_correct_indices = gather_results(t_correct_index, global_rank, gather_sizes, world)
+
+    return aggregated_top10_preds, aggregated_correct_indices
+
+
+def validate(valid_dataset: Wiki90MValidationDataset, valid_dataloader: DataLoader, model, global_rank: int,
+             local_rank: int, gather_sizes: list, num_batches: int = None, world=None):
+    evaluator = WikiKG90MEvaluator()
+    top10_preds, correct_indices = run_inference(valid_dataset, valid_dataloader, model, global_rank, local_rank,
+                                                 gather_sizes, num_batches, world)
     if global_rank == 0:
-        input_dict = {'h,r->t': {'t_pred_top10': aggregated_top10_preds.cpu().numpy(), 't_correct_index': aggregated_correct_indices.cpu().numpy()}}
+        input_dict = {'h,r->t': {'t_pred_top10': top10_preds.cpu().numpy(), 't_correct_index': correct_indices.cpu().numpy()}}
         result_dict = evaluator.eval(input_dict)
         return result_dict
     else:
         return None
 
 
-def gather_results(t_pred_top10: torch.Tensor, t_correct_index: torch.Tensor, global_rank, gather_sizes, world):
-    gather_list_pred = []
-    gather_list_correct = []
+def test(test_dataset: Wiki90MTestDataset, test_dataloader: DataLoader, model, global_rank: int,
+             local_rank: int, gather_sizes: list, num_batches: int = None, world=None):
+    evaluator = WikiKG90MEvaluator()
+    top10_preds, _ = run_inference(test_dataset, test_dataloader, model, global_rank, local_rank, gather_sizes,
+                                   num_batches, world)
+    input_dict = {}
+    input_dict['h,r->t'] = {'t_pred_top10': top10_preds}
+    evaluator.save_test_submission(input_dict=input_dict, dir_path=FLAGS.test_save_dir)
+
+
+def gather_results(data: torch.Tensor, global_rank, gather_sizes, world):
+    gather_list = []
 
     for size in gather_sizes:
-        gather_list_pred.append(torch.empty(size, 10, dtype=t_pred_top10.dtype, device=global_rank))
-        gather_list_correct.append(torch.empty(size, dtype=t_correct_index.dtype, device=global_rank))
+        gather_list.append(torch.empty(size, *data.shape[1:], dtype=data.dtype, device=global_rank))
 
     if global_rank == 0:
-        gather_list_pred[0] = t_pred_top10
-        gather_list_correct[0] = t_correct_index
+        gather_list[0] = data
 
         for p in range(1, world.size()):
-            dist.recv(gather_list_pred[p], src=p, group=world)
-            dist.recv(gather_list_correct[p], src=p, group=world)
+            dist.recv(gather_list[p], src=p, group=world)
     else:
-        dist.send(t_pred_top10, dst=0, group=world)
-        dist.send(t_correct_index, dst=0, group=world)
+        dist.send(data, dst=0, group=world)
 
-    return torch.cat(gather_list_pred, dim=0), torch.cat(gather_list_correct, dim=0)
+    return torch.cat(gather_list, dim=0)
 
 
 def main(argv):
@@ -317,7 +341,7 @@ def main(argv):
 
         if FLAGS.edge_attention and FLAGS.relation_scoring:
             raise Exception("Only one of relation scoring or edge attention can be enabled!")
-        if FLAGS.inference_only:
+        if FLAGS.validation_only or FLAGS.test_only:
             inference_only(grank, FLAGS.local_rank, world)
         else:
             train(grank, FLAGS.local_rank, world)
